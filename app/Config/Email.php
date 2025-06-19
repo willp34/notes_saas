89bd7a8bc853dada6328f8f41648dcec882d931a
willp34 <35201865+willp34@users.noreply.github.com>
2025-06-19 10:57:39 +0100

    Update Email.php

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,7 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'william_pritchard@lekka-media.co.uk';
+    public string $fromEmail  = '*%%%%%©©©¥';
     public string $fromName   = 'William Pritchard';
     public string $recipients = '';
 
